002b5b5e0e827e11e85e1664880d0134fda80065
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-03-21 11:39:37 +0530

    Add files via upload

--- atm.py
+++ atm.py
@@ -1,9 +1,13 @@
+money=10000
+
 class  Atm:
     def __init__(self,cardno,pinno) :
         self.cardno=cardno
         self.pinno=pinno
     def cash(self):
        print("Money for cash withdrawl")
+       nemMoney=money-5000
+       print("Money after Withdrawl is",nemMoney)   
     def cash1(self):
        print("Pin Number")
 atm=Atm("1000","1")
